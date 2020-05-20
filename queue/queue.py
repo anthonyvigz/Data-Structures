87f305f6b3ecd587e5e41ceb651965a266c581a9
anthonyvigz <anthonyvigz@gmail.com>
2020-05-20 18:55:18 -0400

    completed queue class

--- queue/queue.py
+++ queue/queue.py
@@ -16,13 +16,15 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        for item in self.storage:
+            self.size += 1
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.insert(0, value)
 
     def dequeue(self):
-        pass
+        return self.storage.pop()
